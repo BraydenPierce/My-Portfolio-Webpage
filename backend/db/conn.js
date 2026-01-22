d0fe7733b1b0960ab9f40ee4b4386931786bc3ac
Brayden Pierce <braydenpierce@mail.weber.edu>
2026-01-21 18:51:47 -0700

    MongoDB cluster created. server.js will not MongoStore.create

--- backend/db/conn.js
+++ backend/db/conn.js
@@ -4,37 +4,37 @@ const uri = process.env.ATLAS_URI;
 let _db;
 
 module.exports = {
-    connectToServer: function (callback) {
-        console.log("Attempting to connect...");
-        // Create a MongoClient with a MongoClientOptions object to set the Stable API Version
-        const client = new MongoClient(uri, {
-            serverApi: {
-                version: ServerApiVersion.v1,
-                strict: true,
-                depricationErrors: true,
-            },
-        });
-        async function run() {
-            try {
-                // Connect the client to the server
-                await client.connect();
-                // Send a ping to confirm a successful connection
-                await client.db("admin").command({ ping: 1 });  // TODO: Check if admin is okay for deployment
-                console.log(
-                    "Pinged your deployment. You successfully connected to MongoDB!"
-                );
-                _db = client.db("test"); // TODO: Correct name
-                console.log("Successfully connected to speedGame collection");
-            } finally {
-                // Ensures that the client will close when you finish/error
-                console.log("Closing the client");
-                await client.close();
-            }
-        }
-        run().catch(console.dir);
-    },
+  connectToServer: function (callback) {
+    console.log("Attempting to connect...");
+    // Create a MongoClient with a MongoClientOptions object to set the Stable API Version
+    const client = new MongoClient(uri, {
+      serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        depricationErrors: true,
+      },
+    });
+    async function run() {
+      try {
+        // Connect the client to the server
+        await client.connect();
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 }); // TODO: Check if admin is okay for deployment
+        console.log(
+          "Pinged your deployment. You successfully connected to MongoDB!",
+        );
+        _db = client.db("test");
+        console.log("Successfully connected to speedGame collection");
+      } finally {
+        // Ensures that the client will close when you finish/error
+        console.log("Closing the client");
+        await client.close();
+      }
+    }
+    run().catch(console.dir);
+  },
 
-    getDb: function () {
-        return _db;
-    },
+  getDb: function () {
+    return _db;
+  },
 };
---
+++ backend/server.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+
+// Used to store cookies with Mongo
+const session = require("express-session");
+const MongoStore = require("connect-mongo");
+
+require("dotenv").config({ path: "./config.env" });
+
+const port = process.env.PORT;
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials: true,
+    optionsSuccessStatus: 204,
+    allowedHeaders: ["Content-Type", "Authorization"],
+  }),
+);
+
+app.use(
+  session({
+    secret: "keyboard cat",
+    saveUninitialized: false, // don't create sessions until something is stored
+    resave: false, // don't save session if unmodified
+    store: MongoStore.create({
+      mongoUrl: process.env.ATLAS_URI,
+    }),
+  }),
+);
+
+// Get MongoDB driver connection
+const dbo = require("./db/conn");
+
+app.use(express.json());
+
+// API's
+app.use(require("./routes/auth"));
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on ${port}`);
+  dbo.connectToServer(function (err) {
+    if (err) {
+      console.err(err);
+    }
+  });
+  console.log(`Server is running on port ${port}`);
+});
