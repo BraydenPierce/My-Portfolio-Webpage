7978e4fa23292444350f3635683787c5cd17e819
Brayden Pierce <braydenpierce@mail.weber.edu>
2026-01-26 14:28:40 -0700

    More pages created and routed through Navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,6 +6,11 @@ import { Route, Routes } from "react-router-dom";
 import axios from "axios";
 import NavBar from "./components/Navbar";
 import Home from "./pages/home";
+import Xp from "./pages/xp";
+import Portfolio from "./pages/portfolio";
+import Contact from "./pages/contact";
+import Login from "./pages/login";
+import Register from "./pages/register";
 
 // send cookies every request (need for sessions to work)
 axios.defaults.withCredentials = true;
@@ -16,6 +21,11 @@ const App = () => {
       <NavBar />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/experience" element={<Xp />} />
+        <Route path="/portfolio" element={<Portfolio />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </>
   );
---
+++ frontend/src/pages/xp.jsx
@@ -0,0 +1,5 @@
+const Xp = () => {
+  return <h1>Experience Page</h1>;
+};
+
+export default Xp;
