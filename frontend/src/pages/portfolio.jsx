0838d0528176e120e2e27b297110487bbb34d08e
Brayden Pierce <braydenpierce@mail.weber.edu>
2026-01-31 12:46:03 -0700

    rough idea for displaying github projects

--- frontend/src/pages/portfolio.jsx
+++ frontend/src/pages/portfolio.jsx
@@ -1,5 +1,29 @@
+import Card from "react-bootstrap/Card";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import Container from "react-bootstrap/Container";
+
 const Portfolio = () => {
-  return <h1>Portfolio Page</h1>;
+  return (
+    <Container className="mt-4">
+      <Row xs={1} md={2} className="g-4">
+        {Array.from({ length: 4 }).map((_, idx) => (
+          <Col key={idx}>
+            <Card>
+              <Card.Body>
+                <Card.Title>Project Title</Card.Title>
+                <Card.Text>
+                  This is a longer card with supporting text below as a natural
+                  lead-in to additional content. This content is a little bit
+                  longer.
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+        ))}
+      </Row>
+    </Container>
+  );
 };
 
 export default Portfolio;
