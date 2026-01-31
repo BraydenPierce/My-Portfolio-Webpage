4c828e31bba1c83d92a19de75b7604b985ea1d43
Brayden Pierce <braydenpierce@mail.weber.edu>
2026-01-31 11:22:40 -0700

    login form initalization

--- frontend/src/pages/login.jsx
+++ frontend/src/pages/login.jsx
@@ -1,5 +1,26 @@
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+
 const Login = () => {
-  return <h1>Login Page</h1>;
+  return (
+    <Form className="p-5">
+      <Form.Group className="mb-3" controlId="formBasicUsername">
+        <Form.Label>Username</Form.Label>
+        <Form.Control type="text" placeholder="Enter Username" />
+        <Form.Text className="text-muted">
+          Enter your username to login
+        </Form.Text>
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label>Password</Form.Label>
+        <Form.Control type="password" placeholder="Password" />
+      </Form.Group>
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+    </Form>
+  );
 };
 
 export default Login;
