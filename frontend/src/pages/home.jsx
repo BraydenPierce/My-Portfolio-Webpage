8fd99815363eff21acf0e9f590de47a90e1dffa1
Brayden Pierce <braydenpierce@mail.weber.edu>
2026-02-01 15:12:20 -0700

    Testing a homepage layouts

--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -1,10 +1,17 @@
-import { Container, Row } from "react-bootstrap";
+import { Container, Row, Col, Image } from "react-bootstrap";
+import headshot from "../assets/imgs/headshot.JPG";
 
 const Home = () => {
   return (
     <Container className="mt-4">
       <Row>
-        <h1>Hello!</h1>
+        <Col>
+          <Image src={headshot} thumbnail />
+        </Col>
+        <Col>
+          <h1>Hello!</h1>
+          <p>Placeholder paragraph introducing myself</p>
+        </Col>
       </Row>
     </Container>
   );
