063821b518f40354ffdc673d3361d8eb155a35fc
Brayden Pierce <braydenpierce@mail.weber.edu>
2026-02-13 18:17:54 -0700

    styling update

--- frontend/src/pages/contact.jsx
+++ frontend/src/pages/contact.jsx
@@ -1,13 +1,24 @@
-import { Container, Row } from "react-bootstrap";
+import { Container, Row, Image, Col } from "react-bootstrap";
+import email from "../assets/imgs/icon-email.png";
 
 const Contact = () => {
   return (
     <Container className="mt-4">
       <Row>
-        <h1>Contact</h1>
+        <h1>Get in contact with me!</h1>
+      </Row>
+      <Row>
+        <Col>
+          <Image src={email} roundedCircle style={{ width: "64px", height: "64px"}}/>
+        </Col>
+        <Col>
+          <a >piercebrayden27@gmail.com</a>
+        </Col>
       </Row>
     </Container>
   );
 };
+
+// https://stackoverflow.com/questions/63782544/react-open-mailto-e-mail-client-onclick-with-body-from-textarea
 
 export default Contact;
--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -9,8 +9,14 @@ const Home = () => {
           <Image src={bray} thumbnail />
         </Col>
         <Col>
-          <h1>Hello!</h1>
-          <p>Placeholder paragraph introducing myself</p>
+          <h1>Hello and welcome!</h1>
+          <p>
+            My name is Brayden Pierce and I am a Software Developer. I am passionate about
+            creating tools for the benefit of health, happiness, and creative expression.
+            Look around my website to see some of my projects, hobbies, and ways of
+            contacting me!
+          </p>
+          <h2>Enjoy!</h2>
         </Col>
       </Row>
     </Container>
