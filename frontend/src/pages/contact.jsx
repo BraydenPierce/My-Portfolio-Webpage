f0d097baae0a8ba2938ad2df02a427933e61cb1a
Brayden Pierce <braydenpierce@mail.weber.edu>
2026-02-21 13:44:14 -0700

    Added a link to my LinkedIn profile

--- frontend/src/pages/contact.jsx
+++ frontend/src/pages/contact.jsx
@@ -1,18 +1,19 @@
 import { Container, Row, Image, Col } from "react-bootstrap";
 import email from "../assets/imgs/icon-email.png";
+import linkedin from "../assets/imgs/linkedin.png"
 import ButtonMailto from "../components/ButtonMailTo";
 
 const Contact = () => {
   return (
-    <Container className="mt-4">
+    <Container className="mt-4 fs-4">
       <Row>
         <h1>Get in contact with me!</h1>
       </Row>
-      <Row className="align-items-center ms-3">
+      <Row className="align-items-center m-3">
         <Col xs="auto" className="d-flex">
           <Image
             src={email}
-            roundedCircle
+
             style={{ width: "64px", height: "64px" }}
           />
         </Col>
@@ -23,8 +24,33 @@ const Contact = () => {
           />
         </Col>
       </Row>
+      <Row className="align-items-center m-3">
+        <Col xs="auto" className="d-flex">
+          <a
+            href="https://www.linkedin.com/in/brayden-pierce-a4111a260/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <Image
+              src={linkedin}
+              style={{ width: "64px", height: "64px" }}
+            />
+          </a>
+
+        </Col>
+        <Col xs="auto" className="d-flex">
+          <a
+            href="https://www.linkedin.com/in/brayden-pierce-a4111a260/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            View my LinkedIn
+          </a>
+        </Col>
+      </Row>
     </Container>
   );
 };
 
+// https://github.com/BraydenPierce
 export default Contact;
