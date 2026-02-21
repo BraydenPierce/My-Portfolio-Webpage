13a0279b27ab105f01f45070a8a63791be1f0227
Brayden Pierce <braydenpierce@mail.weber.edu>
2026-02-21 13:25:26 -0700

    Better formating

--- frontend/src/pages/contact.jsx
+++ frontend/src/pages/contact.jsx
@@ -8,12 +8,19 @@ const Contact = () => {
       <Row>
         <h1>Get in contact with me!</h1>
       </Row>
-      <Row>
-        <Col sm={8}>
-          <Image src={email} roundedCircle style={{ width: "64px", height: "64px"}}/>
+      <Row className="align-items-center ms-3">
+        <Col xs="auto" className="d-flex">
+          <Image
+            src={email}
+            roundedCircle
+            style={{ width: "64px", height: "64px" }}
+          />
         </Col>
-        <Col sm={4}>
-          <ButtonMailto label="Write me an E-Mail" mailto="mailto:piercebrayden27@gmail.com" />
+        <Col xs="auto" className="d-flex">
+          <ButtonMailto
+            label="Write me an E-Mail"
+            mailto="mailto:piercebrayden27@gmail.com"
+          />
         </Col>
       </Row>
     </Container>
