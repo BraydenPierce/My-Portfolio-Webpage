9136b1f21bfb6a6c680d736356a2e8705446ee01
Brayden Pierce <braydenpierce@mail.weber.edu>
2026-02-22 20:31:34 -0700

    Testing some more layout options

--- frontend/src/pages/hobbies.jsx
+++ frontend/src/pages/hobbies.jsx
@@ -1,9 +1,89 @@
-import { Container, Row, Image, Col } from "react-bootstrap";
+import { Container, Row } from "react-bootstrap";
+import Carousel from 'react-bootstrap/Carousel';
+import Image from "react-bootstrap/Image";
+import dracula from "../assets/imgs/dracula.jpg";
 
 const Hobbies = () => {
   return (
-    <Container className="fs-4 mt-4 bg-body-tertiary shadow rounded-3">
-      <h1>hobbies</h1>
+    <Container className="mt-4 bg-body-tertiary shadow rounded-3">
+      <Row>
+        <h1>Reading</h1>
+        <Carousel className="m-4">
+          <Carousel.Item>
+            <div className="d-flex justify-content-center">
+              <Image src={dracula} style={{ maxWidth: "300px", height: "auto", margin: "20px"}}/>
+            </div>
+            <Carousel.Caption>
+              <h3>First slide label</h3>
+              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <Carousel.Caption>
+              <h3>Second slide label</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <Carousel.Caption>
+              <h3>Third slide label</h3>
+              <p>
+                Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+              </p>
+            </Carousel.Caption>
+          </Carousel.Item>
+        </Carousel>
+      </Row>
+      <Row>
+        <h1>Videogames</h1>
+        <Carousel className="m-4">
+          <Carousel.Item>
+            <Carousel.Caption>
+              <h3>First slide label</h3>
+              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <Carousel.Caption>
+              <h3>Second slide label</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <Carousel.Caption>
+              <h3>Third slide label</h3>
+              <p>
+                Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+              </p>
+            </Carousel.Caption>
+          </Carousel.Item>
+        </Carousel>
+      </Row>
+      <Row>
+        <h1>DnD</h1>
+        <Carousel className="m-4">
+          <Carousel.Item>
+            <Carousel.Caption>
+              <h3>First slide label</h3>
+              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <Carousel.Caption>
+              <h3>Second slide label</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <Carousel.Caption>
+              <h3>Third slide label</h3>
+              <p>
+                Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+              </p>
+            </Carousel.Caption>
+          </Carousel.Item>
+        </Carousel>
+      </Row>
     </Container>
   );
 };
