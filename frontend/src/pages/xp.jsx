fa183695fefeca37dddf5de43e6f8b4ae7ecf88d
Brayden Pierce <braydenpierce@mail.weber.edu>
2026-02-22 19:36:22 -0700

    Added a link to download my resume

--- frontend/src/pages/xp.jsx
+++ frontend/src/pages/xp.jsx
@@ -1,10 +1,25 @@
-import { Container } from "react-bootstrap";
+import { Container, Button, Row, Col } from "react-bootstrap";
+import resume from "../assets/resume.pdf";
 
 const Xp = () => {
   return (
     <Container className="mt-4">
-      <h1>Education:</h1>
-      <h1>Work Experience:</h1>
+      <Row>
+        <h1>Education:</h1>
+      </Row>
+      <Row>
+        <h1>Work Experience:</h1>
+      </Row>
+      <Row className="align-items-center justify-content-center m-3">
+        <Col xs="auto" className="d-flex">
+          <h4>Download my resume: </h4>
+        </Col>
+        <Col xs="auto" className="d-flex">  
+          <Button variant="warning" href={resume} target="_blank" rel="noopener noreferrer">
+            Download PDF
+          </Button>
+        </Col>
+      </Row>
     </Container>
   );
 };
