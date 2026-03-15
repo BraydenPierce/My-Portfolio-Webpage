89989d45b8177b7d75804feaf4aee09ba647be14
Brayden Pierce <braydenpierce@mail.weber.edu>
2026-03-15 14:05:48 -0600

    update: ONC_Hours 11->14

--- frontend/src/pages/xp.jsx
+++ frontend/src/pages/xp.jsx
@@ -2,7 +2,7 @@ import { Container, Button, Row, Col } from "react-bootstrap";
 import Accordion from "react-bootstrap/Accordion"
 import resume from "../assets/resume.pdf";
 
-const ONC_Hours = 11;
+const ONC_Hours = 14;
 
 const Xp = () => {
   return (
