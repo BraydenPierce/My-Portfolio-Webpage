6180033550f028284e6563f46981bd75f9118844
Brayden Pierce <braydenpierce@mail.weber.edu>
2026-04-07 15:40:50 -0600

    feat: wrapped home body text in a container for better looking format

--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -12,52 +12,54 @@ const Home = () => {
           <Image src={bray} thumbnail className="border-tertiary"/>
         </Col>
         <Col>
-          <h1 className="text-secondary">Hello and welcome!</h1>
-          <p className="">
-            My name is Brayden Pierce and I am a Software Developer. I am passionate about
-            creating tools for the benefit of health, happiness, and creative expression.
-            I specialize in Full-Stack Web Development but I am always happy to branch
-            out and learn new things!
-          </p>
-          <p>
-            Look around my website to see some of my projects, hobbies, and ways of
-            contacting me!
-          </p>
-          <h2 className="text-secondary">Enjoy!</h2>
-          <Row className="align-items-center justify-content-center m-3 pb-4">
-            <Col xs="auto" className="d-flex">
-              <a href="mailto:piercebrayden27@gmail.com">
-                <Image
-                  src={email}
-                  style={{ width: "64px", height: "64px" }}
-                />
-              </a>
-            </Col>
-            <Col xs="auto" className="d-flex">
-              <a
-                href="https://www.linkedin.com/in/brayden-pierce-a4111a260/"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <Image
-                  src={linkedin}
-                  style={{ width: "64px", height: "64px" }}
-                />
-              </a>
-            </Col>
-            <Col xs="auto" className="d-flex">
-              <a
-                href="https://github.com/BraydenPierce"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <Image
-                  src={github}
-                  style={{ width: "64px", height: "64px" }}
-                />
-              </a>
-            </Col>
-          </Row>
+          <Container className="bg-primary rounded-3 p-2">
+            <h1 className="d-flex justify-content-center">Hello and welcome!</h1>
+            <p className="">
+              My name is Brayden Pierce and I am a Software Developer. I am passionate about
+              creating tools for the benefit of health, happiness, and creative expression.
+              I specialize in Full-Stack Web Development but I am always happy to branch
+              out and learn new things!
+            </p>
+            <p>
+              Look around my website to see some of my projects, hobbies, and ways of
+              contacting me!
+            </p>
+            <h2 className="d-flex justify-content-center">Enjoy!</h2>
+            <Row className="align-items-center justify-content-center m-3 pb-4">
+              <Col xs="auto" className="d-flex">
+                <a href="mailto:piercebrayden27@gmail.com">
+                  <Image
+                    src={email}
+                    style={{ width: "64px", height: "64px" }}
+                  />
+                </a>
+              </Col>
+              <Col xs="auto" className="d-flex">
+                <a
+                  href="https://www.linkedin.com/in/brayden-pierce-a4111a260/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <Image
+                    src={linkedin}
+                    style={{ width: "64px", height: "64px" }}
+                  />
+                </a>
+              </Col>
+              <Col xs="auto" className="d-flex">
+                <a
+                  href="https://github.com/BraydenPierce"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <Image
+                    src={github}
+                    style={{ width: "64px", height: "64px" }}
+                  />
+                </a>
+              </Col>
+            </Row>
+          </Container>
         </Col>
       </Row>
     </Container>
