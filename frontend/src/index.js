8cbafc0784c7a4adb858d9c9ded9c9f1218bc308
Brayden Pierce <braydenpierce@mail.weber.edu>
2026-04-18 13:47:05 -0600

    dev: turned on webvitals and added comment

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,6 +7,8 @@ import reportWebVitals from "./reportWebVitals";
 const container = document.getElementById("root");
 const root = createRoot(container);
 
+// StrictMode is just for development and doesn't effect production;
+// it's the source for double logs.
 root.render(
   <React.StrictMode>
     <BrowserRouter>
@@ -18,4 +20,4 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals(console.log);
